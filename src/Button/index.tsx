aeb44a3225c5ca7883f5640ec4ea5c08b9eab22c
Kelvin-GohKengBoon <kelvin.goh@myfave.com>
2022-02-09 15:43:10 +0800

    Add Eslint & Prettier to format code.

--- src/Button/index.tsx
+++ src/Button/index.tsx
@@ -1,26 +1,34 @@
-import * as React from "react";
-import { Button as AntButton, ButtonProps as AntButtonProps } from "antd";
+import * as React from 'react'
+import { Button as AntButton, ButtonProps as AntButtonProps } from 'antd'
 
 import 'antd/lib/button/style/index.css'
-import "./style.css"
+import './style.css'
 
 export type ButtonProps = AntButtonProps & {
-  iconPlacement ? : "start" | "end";
-};
+  iconPlacement?: 'start' | 'end'
+}
 
 const Button: React.FC<ButtonProps> = (props) => {
-  const {children, ...restProps} = props;
-  return <AntButton {...restProps}>
-    <span className={
-      props.iconPlacement === "start" ? "start" 
-      : (props.iconPlacement !=null ? "end" 
-      : (props.shape == "circle" ? "" : "default"))}>
-      {children}
-    </span>
-  </AntButton>
-};
+  const { children, ...restProps } = props
+  return (
+    <AntButton {...restProps}>
+      <span
+        className={
+          props.iconPlacement === 'start'
+            ? 'start'
+            : props.iconPlacement != null
+            ? 'end'
+            : props.shape === 'circle'
+            ? ''
+            : 'default'
+        }
+      >
+        {children}
+      </span>
+    </AntButton>
+  )
+}
 
-Button.propTypes = {
-};
+Button.propTypes = {}
 
 export default Button;
--- src/Button/stories/index.stories.tsx
+++ src/Button/stories/index.stories.tsx
@@ -1,99 +1,126 @@
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
 
-import {MagnifyingGlass,CaretRight,CaretLeft,Plus} from "phosphor-react";
-import Button,{ ButtonProps } from '../index';
+import { MagnifyingGlass, CaretRight, CaretLeft, Plus } from 'phosphor-react'
+import Button, { ButtonProps } from '../index'
 
 export default {
   title: 'Button',
   component: Button,
-} as ComponentMeta<typeof Button>;
+} as ComponentMeta<typeof Button>
 
-const Template: ComponentStory<typeof Button> = (args: ButtonProps) => <Button {...args} />;
+const Template: ComponentStory<typeof Button> = (args: ButtonProps) => <Button {...args} />
 
-
-export const Primary = Template.bind({});
+export const Primary = Template.bind({})
 
 Primary.args = {
-	label: 'Button',
-  children:"BUTTON",
-  type:"primary",
-  size:"large",
-};
+  label: 'Button',
+  children: 'BUTTON',
+  type: 'primary',
+  size: 'large',
+}
 
-export const PrimaryIconLeft = Template.bind({});
+export const PrimaryIconLeft = Template.bind({})
 
 PrimaryIconLeft.args = {
-	label: 'Button',
-  iconPlacement:"start",
-  children:<><CaretRight size={24} className="iconLeft"/> BUTTON</>,
-  type:"primary",
-  size:"large",
-};
+  label: 'Button',
+  iconPlacement: 'start',
+  children: (
+    <>
+      <CaretRight size={24} className="iconLeft" /> BUTTON
+    </>
+  ),
+  type: 'primary',
+  size: 'large',
+}
 
-export const PrimaryIconRight = Template.bind({});
+export const PrimaryIconRight = Template.bind({})
 
 PrimaryIconRight.args = {
-	label: 'Button',
-  iconPlacement:"end",
-  children:<>BUTTON <MagnifyingGlass size={24} className="iconRight"/></>,
-  type:"primary",
-  size:"large",
-};
+  label: 'Button',
+  iconPlacement: 'end',
+  children: (
+    <>
+      BUTTON <MagnifyingGlass size={24} className="iconRight" />
+    </>
+  ),
+  type: 'primary',
+  size: 'large',
+}
 
-export const Text = Template.bind({});
+export const Text = Template.bind({})
 
 Text.args = {
-	label: 'Button',
-  children:"BUTTON",
-  type:"text",
-  size:"large",
-};
+  label: 'Button',
+  children: 'BUTTON',
+  type: 'text',
+  size: 'large',
+}
 
-export const TextIconLeft = Template.bind({});
+export const TextIconLeft = Template.bind({})
 
 TextIconLeft.args = {
-	label: 'Button',
-  type:"text",
-  iconPlacement:"start",
-  children:<><CaretRight size={24} className="iconLeft"/> BUTTON</>,
-  size:"large",
-};
+  label: 'Button',
+  type: 'text',
+  iconPlacement: 'start',
+  children: (
+    <>
+      <CaretRight size={24} className="iconLeft" /> BUTTON
+    </>
+  ),
+  size: 'large',
+}
 
-export const TextIconRight = Template.bind({});
+export const TextIconRight = Template.bind({})
 
 TextIconRight.args = {
-	label: 'Button',
-  type:"text",
-  iconPlacement:"end",
-  children:<>BUTTON <MagnifyingGlass size={24} className="iconRight"/></>,
-  size:"large",
-};
+  label: 'Button',
+  type: 'text',
+  iconPlacement: 'end',
+  children: (
+    <>
+      BUTTON <MagnifyingGlass size={24} className="iconRight" />
+    </>
+  ),
+  size: 'large',
+}
 
-export const IconSearch = Template.bind({});
+export const IconSearch = Template.bind({})
 
 IconSearch.args = {
-	label: 'Button',
-  children:<><MagnifyingGlass size={16} className="circle"/></>,
-  shape:"circle",
-  size:"large",
-};
+  label: 'Button',
+  children: (
+    <>
+      <MagnifyingGlass size={16} className="circle" />
+    </>
+  ),
+  shape: 'circle',
+  size: 'large',
+}
 
-export const IconCaretLeft = Template.bind({});
+export const IconCaretLeft = Template.bind({})
 
 IconCaretLeft.args = {
-	label: 'Button',
-  children:<><CaretLeft size={16} className="circle"/></>,
-  shape:"circle",
-  size:"large",
-};
+  label: 'Button',
+  children: (
+    <>
+      <CaretLeft size={16} className="circle" />
+    </>
+  ),
+  shape: 'circle',
+  size: 'large',
+}
 
-export const IconPlus = Template.bind({});
+export const IconPlus = Template.bind({})
 
 IconPlus.args = {
-	label: 'Button',
-  children:<><Plus size={16} className="circle"/></>,
-  type:"primary",
-  shape:"circle",
-  size:"large",
-};
+  label: 'Button',
+  children: (
+    <>
+      <Plus size={16} className="circle" />
+    </>
+  ),
+  type: 'primary',
+  shape: 'circle',
+  size: 'large',
+}
