2f764924a3124bfb94361f80008465e2e4c011fd
Kelvin-GohKengBoon <kelvin.goh@myfave.com>
2022-02-14 14:46:32 +0800

    Add Checkbox Component

--- src/Checkbox/stories/index.stories.tsx
+++ src/Checkbox/stories/index.stories.tsx
@@ -5,6 +5,7 @@ export default {
   title: 'Checkbox',
   component: Checkbox
 } as ComponentMeta<typeof Checkbox>
+
 const Template: ComponentStory<typeof Checkbox> = (args: CheckboxProps) => <Checkbox {...args} />
 export const Primary = Template.bind({})
 Primary.args = {
