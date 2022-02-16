0ff7bbec322b5011efe7195fe37d6bdadadb91de
Kelvin-GohKengBoon <kelvin.goh@myfave.com>
2022-02-16 11:07:12 +0800

    resolve conflicts

--- src/Checkbox/stories/index.stories.tsx
+++ src/Checkbox/stories/index.stories.tsx
@@ -7,31 +7,41 @@ export default {
 } as ComponentMeta<typeof Checkbox>
 
 const Template: ComponentStory<typeof Checkbox> = (args: CheckboxProps) => <Checkbox {...args} />
+
 export const Primary = Template.bind({})
+
 Primary.args = {
   label: 'Checkbox',
   children: 'Checkbox'
 }
+
 export const PrimaryChecked = Template.bind({})
+
 PrimaryChecked.args = {
   label: 'Checkbox',
   children: 'Checkbox',
   defaultChecked: true
 }
+
 export const Disabled = Template.bind({})
+
 Disabled.args = {
   label: 'Checkbox',
   children: 'Checkbox',
   disabled: true
 }
+
 export const DisabledChecked = Template.bind({})
+
 DisabledChecked.args = {
   label: 'Checkbox',
   children: 'Checkbox',
   defaultChecked: true,
   disabled: true
 }
+
 export const CheckAll = Template.bind({})
+
 CheckAll.args = {
   label: 'Checkbox',
   children: 'Check All',
