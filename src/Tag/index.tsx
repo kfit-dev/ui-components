a7ac56ddab2e2c8c215b3b83d34236dd959fe57b
Aladdin <aladdin-2010@hotmail.com>
2022-02-24 14:23:26 +0800

    removed isCustom prop from tag

--- src/Tag/index.tsx
+++ src/Tag/index.tsx
@@ -1,22 +1,17 @@
 import * as React from 'react'
 import { Tag as AntTag, TagProps as AntTagProps } from 'antd'
-import classNames from 'classnames'
 import { X } from 'phosphor-react'
 
 import 'antd/lib/tag/style/index.css'
 import './style.css'
 
-export type TagProps = AntTagProps & {
-  isCustom: boolean
-}
+export type TagProps = AntTagProps & {}
 
-const Tag: React.FC<TagProps> = ({ isCustom, className, color, ...rest }) => {
-  const classes = classNames(isCustom && 'custom', !color && 'none', className)
-  return <AntTag className={classes} color={color} {...rest} />
+const Tag: React.FC<TagProps> = props => {
+  return <AntTag {...props} />
 }
 
 Tag.defaultProps = {
-  isCustom: true,
   closeIcon: <X size={12} />
 }
 
