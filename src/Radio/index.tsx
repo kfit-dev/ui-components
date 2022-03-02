36a47b38817a9aa27682a1a5ecd33d90954964f9
Kelvin-GohKengBoon <97870401+Kelvin-GohKengBoon@users.noreply.github.com>
2022-03-02 11:31:51 +0800

    Feature/radio component (#13)
    * Add Radio Button Component
    
    * Rename Primary Radio to Basic Radio
    
    * Fix the Radio Styling
    
    * Update Font Color

--- src/Radio/index.tsx
+++ src/Radio/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { Radio as AntRadio, RadioProps as AntRadioProps } from 'antd'
 
+import 'antd/lib/radio/style/index.css'
 import './style.css'
 
 export type RadioProps = AntRadioProps
