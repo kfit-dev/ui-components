64d1dbd41723ccec3c8727262941b6b64dfa2f58
Kelvin-GohKengBoon <kelvin.goh@myfave.com>
2022-03-09 14:34:28 +0800

    Move component style file to main style file

--- src/Input/index.tsx
+++ src/Input/index.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react'
 import { default as AntInput, InputProps as AntInputProps, SearchProps as AntSearchProps } from 'antd/lib/input'
 
-import '../assets/style/form-item.css'
-import 'antd/lib/input/style/index.css'
 import './style.css'
 
 export type InputProps = AntInputProps
