0c3947c766eeb15f682dd5a26f1233b5e1f5eb69
Kelvin-GohKengBoon <kelvin.goh@myfave.com>
2022-03-09 15:12:14 +0800

    Move components style to main style file

--- src/Tooltip/index.tsx
+++ src/Tooltip/index.tsx
@@ -1,9 +1,6 @@
 import * as React from 'react'
 import { Tooltip as AntTooltip, TooltipProps as AntTooltipProps } from 'antd'
 
-import 'antd/lib/style/default.css'
-import 'antd/lib/tooltip/style/index.css'
-import 'antd/lib/empty/style/index.css'
 import './style.css'
 
 export type TooltipProps = AntTooltipProps
