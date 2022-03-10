0445b007cc9fdd6dfac037014ee599dcad7725a3
naimdanialjohar <37859609+naimdanialjohar@users.noreply.github.com>
2022-03-10 11:20:42 +0800

    moved antd css into component styles file

--- src/Badge/index.tsx
+++ src/Badge/index.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
 import { Badge as AntBadge, BadgeProps as AntBadgeProps } from 'antd';
-
-import 'antd/lib/badge/style/index.css';
 import './style.css';
 
 export type BadgeProps = AntBadgeProps;
