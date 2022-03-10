ab157af0f2cf673d929c9e72570fa5c9d0c24aa7
naimdanialjohar <37859609+naimdanialjohar@users.noreply.github.com>
2022-03-10 11:31:36 +0800

    allow to pass classname via props, added comment to css class

--- src/Badge/index.tsx
+++ src/Badge/index.tsx
@@ -1,12 +1,19 @@
 import * as React from 'react';
 import { Badge as AntBadge, BadgeProps as AntBadgeProps } from 'antd';
+import classNames from 'classnames';
+
 import './style.css';
 
 export type BadgeProps = AntBadgeProps;
 
-const Badge: React.FC<BadgeProps> = props => {
+const Badge: React.FC<BadgeProps> = ({ className, ...props }) => {
   const { dot } = props;
-  return <AntBadge className={dot ? 'status-badge' : ''} {...props} />;
+  return (
+    <AntBadge
+      className={classNames(dot ? 'status-badge' : '', className)}
+      {...props}
+    />
+  );
 };
 
 Badge.defaultProps = {};
