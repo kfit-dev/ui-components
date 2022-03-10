8388fb382213d0b7681ebf63861b87e31c4e35fc
naimdanialjohar <37859609+naimdanialjohar@users.noreply.github.com>
2022-03-10 12:09:28 +0800

    removed unnecessary css classes

--- src/Badge/index.tsx
+++ src/Badge/index.tsx
@@ -1,19 +1,11 @@
 import * as React from 'react';
 import { Badge as AntBadge, BadgeProps as AntBadgeProps } from 'antd';
-import classNames from 'classnames';
-
 import './style.css';
 
 export type BadgeProps = AntBadgeProps;
 
-const Badge: React.FC<BadgeProps> = ({ className, ...props }) => {
-  const { dot } = props;
-  return (
-    <AntBadge
-      className={classNames(dot ? 'status-badge' : '', className)}
-      {...props}
-    />
-  );
+const Badge: React.FC<BadgeProps> = props => {
+  return <AntBadge {...props} />;
 };
 
 Badge.defaultProps = {};
