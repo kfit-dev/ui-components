6cf0b7ef533274c3b1e0844dc84a15293acb326d
Kelvin-GohKengBoon <97870401+Kelvin-GohKengBoon@users.noreply.github.com>
2022-03-11 14:45:09 +0800

    Feature/Popover Component (#23)
    * Add Popover Component
    
    * Add Button Styling for Demostration Purpose
    
    * Remove redundant code
    
    * Add default.css
    
    * Update popover styling
    
    Co-authored-by: AladdinNullptr <83447228+AladdinNullptr@users.noreply.github.com>

--- src/index.tsx
+++ src/index.tsx
@@ -4,3 +4,4 @@ export { default as AvatarGroup, AvatarGroupProps } from './AvatarGroup';
 export { default as Button } from './Button';
 export { default as Radio } from './Radio';
 export { default as Tag } from './Tag';
+export { default as Popover } from './Popover'
