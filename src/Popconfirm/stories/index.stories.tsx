1f36d262c154c19c3b1b8235da9bd81197f0e806
Kelvin-GohKengBoon <kelvin.goh@myfave.com>
2022-03-11 15:46:23 +0800

    Initiate pop confirm component

---
+++ src/Popconfirm/stories/index.stories.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { Button } from 'antd'
+import { WarningCircle } from 'phosphor-react'
+import { default as Popconfirm, PopconfirmProps } from '../index'
+
+export default {
+  title: 'Popconfirm',
+  component: Popconfirm
+} as ComponentMeta<typeof Popconfirm>
+
+const Template: ComponentStory<typeof Popconfirm> = (args: PopconfirmProps) => {
+  return (
+    <Popconfirm style={{ margin: '120px' }} {...args}>
+      <Button type="primary" style={{ margin: '200px' }}>
+        Click Me
+      </Button>
+    </Popconfirm>
+  )
+}
+
+export const Top = Template.bind({})
+
+Top.args = {
+  label: 'Popconfirm',
+  title: 'Are you sure you wish to delete this task',
+  placement: 'top',
+  icon: <WarningCircle size={16} />,
+  onConfirm: { confirm },
+  okText: 'Button',
+  cancelText: 'Button'
+}
+
+export const Bottom = Template.bind({})
+
+Bottom.args = {
+  label: 'Popconfirm',
+  title: 'Are you sure you wish to delete this task',
+  placement: 'bottom',
+  icon: <WarningCircle size={16} />,
+  onConfirm: { confirm },
+  okText: 'Button',
+  cancelText: 'Button'
+}
