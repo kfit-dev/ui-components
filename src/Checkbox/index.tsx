837f6ecf0775ecb52e78889c0ec2af070776f7c3
Kelvin-GohKengBoon <kelvin.goh@myfave.com>
2022-03-14 09:46:00 +0800

    Code Formatting & Enchancement

--- src/Checkbox/index.tsx
+++ src/Checkbox/index.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react'
 import { Checkbox as AntCheckbox, CheckboxProps as AntCheckboxProps } from 'antd'
-import 'antd/lib/checkbox/style/index.css'
+
 import './style.css'
 
 export type CheckboxProps = AntCheckboxProps
+
 const Checkbox: React.FC<CheckboxProps> = props => {
   return <AntCheckbox {...props} />
 }
