f33989835d592741e01afd466bb774fc3190864f
Kelvin-GohKengBoon <kelvin.goh@myfave.com>
2022-03-14 11:42:52 +0800

    Add checkbox props into the main index export file

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-export { default as Badge, BadgeProps } from './Badge';
-export { default as Avatar, AvatarProps } from './Avatar';
-export { default as AvatarGroup, AvatarGroupProps } from './AvatarGroup';
-export { default as Button } from './Button';
-export { default as Radio } from './Radio';
-export { default as Tag } from './Tag';
-export { default as Checkbox } from './Checkbox'
+export { default as Badge, BadgeProps } from './Badge'
+export { default as Avatar, AvatarProps } from './Avatar'
+export { default as AvatarGroup, AvatarGroupProps } from './AvatarGroup'
+export { default as Button } from './Button'
+export { default as Radio } from './Radio'
+export { default as Tag } from './Tag'
+export { default as Checkbox, CheckboxProps } from './Checkbox'
 export { default as Popover } from './Popover'
