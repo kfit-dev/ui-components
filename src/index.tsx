082c0251adf1c00dff2672341458a9b2f935340c
naimdanialjohar <37859609+naimdanialjohar@users.noreply.github.com>
2022-03-16 15:54:30 +0800

    created new component Timeline

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 export { default as Empty, EmptyProps } from './Empty';
 export { default as Button, ButtonProps } from './Button';
-export { default as Checkbox, CheckboxProps } from './Checkbox'
-export { default as Tooltip, TooltipProps } from './Tooltip'
+export { default as Checkbox, CheckboxProps } from './Checkbox';
+export { default as Tooltip, TooltipProps } from './Tooltip';
 export { default as Radio, RadioProps } from './Radio';
 export { default as Tag, TagProps } from './Tag';
 export { default as Progress, ProgressProps } from './Progress';
@@ -9,3 +9,8 @@ export { default as Avatar, AvatarProps } from './Avatar';
 export { default as AvatarGroup, AvatarGroupProps } from './AvatarGroup';
 export { default as Badge, BadgeProps } from './Badge';
 export { default as Popover } from './Popover';
+export {
+  default as Timeline,
+  TimelineProps,
+  TimelineItemProps
+} from './Timeline';
