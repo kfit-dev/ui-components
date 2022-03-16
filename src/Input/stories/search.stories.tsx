08c3c5b34a193e0ad5db214f774b612238459ca1
Kelvin-GohKengBoon <97870401+Kelvin-GohKengBoon@users.noreply.github.com>
2022-03-16 12:52:21 +0800

    Feature/input component (#29)
    * Add Input Component
    
    * Remove unused code
    
    * add Search Input
    
    * Add Input Component
    
    * Remove unused code
    
    * add Search Input
    
    * Add export component to main index file
    
    Update Styling of Search Input on Search Icon Style
    
    * Remove merge conflicts
    
    * Fixed Form Label Style
    
    * Move component style file to main style file
    
    * Update border style to focused borderless input
    
    * Remove redundant code & add input props into export file
    
    * Add Search component into main export file

---
+++ src/Input/stories/search.stories.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import { Story, ComponentMeta } from '@storybook/react'
+import { Form, FormItemProps } from 'antd'
+import { Search, SearchProps } from '../index'
+
+export default {
+  title: 'Search',
+  component: Search
+} as ComponentMeta<typeof Search>
+
+type Args = {
+  formItem: FormItemProps
+  input: SearchProps
+}
+
+const Template: Story<Args> = (args: Args) => (
+  <Form.Item style={{ width: '320px' }} {...args.formItem}>
+    <Search {...args.input} />
+  </Form.Item>
+)
+
+export const Basic = Template.bind({})
+
+Basic.args = {
+  input: {
+    label: 'Input',
+    placeholder: 'Placeholder',
+    allowClear: true
+  },
+  formItem: {
+    label: 'Text field title'
+  }
+}
+
+export const SearchError = Template.bind({})
+
+SearchError.args = {
+  input: {
+    label: 'Input',
+    placeholder: 'Placeholder',
+    allowClear: true
+  },
+  formItem: {
+    label: 'Text field title',
+    validateStatus: 'error',
+    help: 'Error text here. No more than one line'
+  }
+}
+
+export const SearchLoading = Template.bind({})
+
+SearchLoading.args = {
+  input: {
+    label: 'Input',
+    placeholder: 'Placeholder',
+    allowClear: true,
+    loading: true
+  },
+  formItem: {
+    label: 'Text field title'
+  }
+}
