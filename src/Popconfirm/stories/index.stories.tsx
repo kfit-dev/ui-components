d28cd32b9a3da993e5be133e8512853c60d9a0e9
Kelvin-GohKengBoon <kelvin.goh@myfave.com>
2022-03-18 14:53:13 +0800

    Add warning,error,info and success state of icon

--- src/Popconfirm/stories/index.stories.tsx
+++ src/Popconfirm/stories/index.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 import { Button } from 'antd'
-import { WarningCircle } from 'phosphor-react'
+import { WarningCircle, XCircle, CheckCircle } from 'phosphor-react'
 import { default as Popconfirm, PopconfirmProps } from '../index'
 
 export default {
@@ -31,13 +31,49 @@ Top.args = {
   cancelText: 'Button'
 }
 
-export const TopIcon = Template.bind({})
+export const TopWarningIcon = Template.bind({})
 
-TopIcon.args = {
+TopWarningIcon.args = {
+  label: 'Popconfirm',
+  title: 'Are you sure you wish to delete this task',
+  placement: 'top',
+  icon: <WarningCircle size={16} color={'#FBBF24'} />,
+  onConfirm: { confirm },
+  okText: 'Button',
+  cancelText: 'Button'
+}
+
+export const TopInfoIcon = Template.bind({})
+
+TopInfoIcon.args = {
   label: 'Popconfirm',
   title: 'Are you sure you wish to delete this task',
   placement: 'top',
   icon: <WarningCircle size={16} />,
+  onConfirm: { confirm },
+  okText: 'Button',
+  cancelText: 'Button'
+}
+
+export const TopErrorIcon = Template.bind({})
+
+TopErrorIcon.args = {
+  label: 'Popconfirm',
+  title: 'Are you sure you wish to delete this task',
+  placement: 'top',
+  icon: <XCircle size={16} color={'#DC2626'} />,
+  onConfirm: { confirm },
+  okText: 'Button',
+  cancelText: 'Button'
+}
+
+export const TopSuccessIcon = Template.bind({})
+
+TopSuccessIcon.args = {
+  label: 'Popconfirm',
+  title: 'Are you sure you wish to delete this task',
+  placement: 'top',
+  icon: <CheckCircle size={16} color={'#10B981'} />,
   onConfirm: { confirm },
   okText: 'Button',
   cancelText: 'Button'
