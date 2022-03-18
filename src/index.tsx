7bf1050d69aecb8c936e87dd2ec2d65879836131
naimdanialjohar <37859609+naimdanialjohar@users.noreply.github.com>
2022-03-18 16:17:45 +0800

    [Develop] new component Timeline (#43)
    * created new component Timeline
    
    * updated font family to inter

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 export { default as Empty, EmptyProps } from './Empty';
 export { default as Button, ButtonProps } from './Button';
-export { default as Checkbox, CheckboxProps } from './Checkbox'
-export { default as Tooltip, TooltipProps } from './Tooltip'
-export { default as Select, SelectProps } from './Select'
+export { default as Checkbox, CheckboxProps } from './Checkbox';
+export { default as Tooltip, TooltipProps } from './Tooltip';
+export { default as Select, SelectProps } from './Select';
 export { default as Radio, RadioProps } from './Radio';
 export { default as Tag, TagProps } from './Tag';
 export { default as Progress, ProgressProps } from './Progress';
@@ -10,7 +10,12 @@ export { default as Avatar, AvatarProps } from './Avatar';
 export { default as AvatarGroup, AvatarGroupProps } from './AvatarGroup';
 export { default as Badge, BadgeProps } from './Badge';
 export { default as Popover } from './Popover';
-export { default as Input, Search, InputProps, SearchProps } from './Input'
+export {
+  default as Timeline,
+  TimelineProps,
+  TimelineItemProps
+} from './Timeline';
+export { default as Input, Search, InputProps, SearchProps } from './Input';
 export {
   default as DatePicker,
   MonthPicker,
@@ -19,4 +24,4 @@ export {
   TimePicker,
   RangePicker,
   QuarterPicker
-} from './DatePicker'
+} from './DatePicker';
