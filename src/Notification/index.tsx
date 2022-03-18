81f66ed2c13626f59186ffc0b4edaa7d0fd81142
naimdanialjohar <37859609+naimdanialjohar@users.noreply.github.com>
2022-03-18 15:52:47 +0800

    updated code based on feedback

--- src/Notification/index.tsx
+++ src/Notification/index.tsx
@@ -9,21 +9,28 @@ export type NotificationProps = NotificationApi;
 const Notification = AntNotification;
 const originalCalls = { ...Notification };
 
+const iconSize = 24;
+
 Notification.info = ({ icon, ...restArgs }: ArgsProps) =>
-  originalCalls.info({ icon: icon || <Info size={20} />, ...restArgs });
+  originalCalls.info({ icon: icon || <Info size={iconSize} />, ...restArgs });
 
 Notification.success = ({ icon, ...restArgs }: ArgsProps) =>
   originalCalls.success({
-    icon: icon || <CheckCircle size={20} />,
+    icon: icon || <CheckCircle size={iconSize} />,
     ...restArgs
   });
 
 Notification.error = ({ icon, ...restArgs }: ArgsProps) =>
-  originalCalls.error({ icon: icon || <XCircle size={20} />, ...restArgs });
+  originalCalls.error({
+    icon: icon || <XCircle size={iconSize} />,
+    duration: 0,
+    ...restArgs
+  });
 
 Notification.warning = ({ icon, ...restArgs }: ArgsProps) =>
   originalCalls.warning({
-    icon: icon || <WarningCircle size={20} />,
+    icon: icon || <WarningCircle size={iconSize} />,
+    duration: 0,
     ...restArgs
   });
 
