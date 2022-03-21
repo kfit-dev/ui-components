fe0aaeeb931c57fb119d0a6ad6e3e20f0ccd1672
naimdanialjohar <37859609+naimdanialjohar@users.noreply.github.com>
2022-03-21 12:18:57 +0800

    rearranged imports

--- src/Alert/stories/index.stories.tsx
+++ src/Alert/stories/index.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { default as Alert, AlertProps, AlertWithButtons } from '../index';
 import Button from '../../Button';
+import { default as Alert, AlertProps, AlertWithButtons } from '../index';
 
 export default {
   title: 'Alert',
