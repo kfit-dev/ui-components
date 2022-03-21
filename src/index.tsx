8cb4d9f5eed94327d918b8da79ff361bbc956b92
naimdanialjohar <37859609+naimdanialjohar@users.noreply.github.com>
2022-03-21 16:13:11 +0800

    [Develop] new component Alert (#40)
    * created new component Alert
    
    * rearranged imports
    
    * update font family to inter
    
    * updated button size and classname
    
    * used buttonProps instead
    
    * refactored code based on feedback
    
    * rearranged imports
    
    * included AlertWithButtons in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -10,12 +10,13 @@ export { default as Avatar, AvatarProps } from './Avatar';
 export { default as AvatarGroup, AvatarGroupProps } from './AvatarGroup';
 export { default as Badge, BadgeProps } from './Badge';
 export { default as Popover } from './Popover';
+export { default as Alert, AlertProps, AlertWithButtons } from './Alert';
+export { default as Input, Search, InputProps, SearchProps } from './Input';
 export {
   default as Timeline,
   TimelineProps,
   TimelineItemProps
 } from './Timeline';
-export { default as Input, Search, InputProps, SearchProps } from './Input';
 export {
   default as DatePicker,
   MonthPicker,
