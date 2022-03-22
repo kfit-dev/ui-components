b56a1fb79a2cde17ee66552bffb2242933021684
Kelvin-GohKengBoon <kelvin.goh@myfave.com>
2022-03-22 10:56:21 +0800

    Add input import and apply auto complete filter

--- src/AutoComplete/index.tsx
+++ src/AutoComplete/index.tsx
@@ -1,19 +1,12 @@
 import * as React from 'react'
 import { AutoComplete as AntAutoComplete, AutoCompleteProps as AntAutoCompleteProps } from 'antd'
 
-import '../assets/style/form-item.css'
-import 'antd/lib/style/default.css'
-import 'antd/lib/select/style/index.css'
-import 'antd/lib/empty/style/index.css'
 import './style.css'
 
-export type AutoCompleteProps = AntAutoCompleteProps & {
-  rounded?: true | false
-}
+export type AutoCompleteProps = AntAutoCompleteProps
 
 const AutoComplete: React.FC<AutoCompleteProps> = props => {
-  const { rounded, ...restProps } = props
-  return <AntAutoComplete className={!rounded ? undefined : 'roundedSearch'} {...restProps}></AntAutoComplete>
+  return <AntAutoComplete {...props}></AntAutoComplete>
 }
 
 export default AutoComplete
