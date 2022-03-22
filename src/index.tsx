f7f73dfbf2f1d1edc0d1495009040c4fa7953e7a
Kelvin-GohKengBoon <kelvin.goh@myfave.com>
2022-03-22 10:58:33 +0800

    Add export file into the main export file

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,18 @@
-export { default as Empty, EmptyProps } from './Empty';
-export { default as Button, ButtonProps } from './Button';
-export { default as Checkbox, CheckboxProps } from './Checkbox';
-export { default as Tooltip, TooltipProps } from './Tooltip';
-export { default as Select, SelectProps } from './Select';
-export { default as Radio, RadioProps } from './Radio';
-export { default as Tag, TagProps } from './Tag';
-export { default as Progress, ProgressProps } from './Progress';
-export { default as Avatar, AvatarProps } from './Avatar';
-export { default as AvatarGroup, AvatarGroupProps } from './AvatarGroup';
-export { default as Badge, BadgeProps } from './Badge';
-export { default as Popover } from './Popover';
-export { default as Alert, AlertProps, AlertWithButtons } from './Alert';
-export { default as Input, Search, InputProps, SearchProps } from './Input';
-export {
-  default as Timeline,
-  TimelineProps,
-  TimelineItemProps
-} from './Timeline';
+export { default as Empty, EmptyProps } from './Empty'
+export { default as Button, ButtonProps } from './Button'
+export { default as Checkbox, CheckboxProps } from './Checkbox'
+export { default as Tooltip, TooltipProps } from './Tooltip'
+export { default as Select, SelectProps } from './Select'
+export { default as Radio, RadioProps } from './Radio'
+export { default as Tag, TagProps } from './Tag'
+export { default as Progress, ProgressProps } from './Progress'
+export { default as Avatar, AvatarProps } from './Avatar'
+export { default as AvatarGroup, AvatarGroupProps } from './AvatarGroup'
+export { default as Badge, BadgeProps } from './Badge'
+export { default as Popover } from './Popover'
+export { default as Alert, AlertProps, AlertWithButtons } from './Alert'
+export { default as Input, Search, InputProps, SearchProps } from './Input'
+export { default as Timeline, TimelineProps, TimelineItemProps } from './Timeline'
 export {
   default as DatePicker,
   MonthPicker,
@@ -27,3 +23,4 @@ export {
   QuarterPicker
 } from './DatePicker';
 export { default as Pagination, PaginationProps } from './Pagination'
+export { default as AutoComplete, AutoCompleteProps } from './AutoComplete'
