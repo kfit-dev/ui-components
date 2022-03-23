5067038aa65f9a9c8bc8045aa363e9053feb384b
AladdinNullptr <83447228+AladdinNullptr@users.noreply.github.com>
2022-03-23 11:48:15 +0800

    [develop] feature/fave 16962 upload component (#45)
    * upload: initial commit
    
    * upload: add action buttons, dragger
    
    * upload: added item list styles
    
    * upload: removed commented code
    
    * upload: fixed drag and drop story
    
    * upload: fixed shallow copying issue and anchor color

--- src/index.tsx
+++ src/index.tsx
@@ -10,8 +10,18 @@ export { default as Avatar, AvatarProps } from './Avatar';
 export { default as AvatarGroup, AvatarGroupProps } from './AvatarGroup';
 export { default as Badge, BadgeProps } from './Badge';
 export { default as Popover } from './Popover';
+export { default as Input, Search, InputProps, SearchProps } from './Input'
+export {
+  default as Upload,
+  UploadProps,
+  Dragger,
+  DraggerProps,
+  UploadButtonContent,
+  UploadButtonContentProps
+} from './Upload'
+export * from 'antd/lib/upload/interface'
+
 export { default as Alert, AlertProps, AlertWithButtons } from './Alert';
-export { default as Input, Search, InputProps, SearchProps } from './Input';
 export {
   default as Timeline,
   TimelineProps,
