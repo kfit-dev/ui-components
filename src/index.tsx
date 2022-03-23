b54a04065558d3faff418e8c7327c674177740fb
naimdanialjohar <37859609+naimdanialjohar@users.noreply.github.com>
2022-03-23 12:13:28 +0800

    [Develop] new function call / component Notification (#44)
    * created new function call Notification
    
    * rearranged imports
    
    * update import path
    
    * updated css based on feedback
    
    * fixed imports
    
    * updated code based on feedback
    
    * added custom close icon to notification
    
    * change type value

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,8 @@ export { default as Avatar, AvatarProps } from './Avatar';
 export { default as AvatarGroup, AvatarGroupProps } from './AvatarGroup';
 export { default as Badge, BadgeProps } from './Badge';
 export { default as Popover } from './Popover';
-export { default as Input, Search, InputProps, SearchProps } from './Input'
+export { default as Notification, NotificationProps } from './Notification';
+export { default as Input, Search, InputProps, SearchProps } from './Input';
 export {
   default as Upload,
   UploadProps,
@@ -18,9 +19,8 @@ export {
   DraggerProps,
   UploadButtonContent,
   UploadButtonContentProps
-} from './Upload'
-export * from 'antd/lib/upload/interface'
-
+} from './Upload';
+export * from 'antd/lib/upload/interface';
 export { default as Alert, AlertProps, AlertWithButtons } from './Alert';
 export {
   default as Timeline,
@@ -36,4 +36,4 @@ export {
   RangePicker,
   QuarterPicker
 } from './DatePicker';
-export { default as Pagination, PaginationProps } from './Pagination'
+export { default as Pagination, PaginationProps } from './Pagination';
