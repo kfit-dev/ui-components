c99c84ad6b9c0d32724956e2b40ca9b5f7929f08
naimdanialjohar <37859609+naimdanialjohar@users.noreply.github.com>
2022-03-28 15:32:06 +0800

    [QA-Staging] created new component Menu (#66)
    * created new component Menu
    
    * testing github pages (#65)
    
    * additional changes
    
    Co-authored-by: AladdinNullptr <83447228+AladdinNullptr@users.noreply.github.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
-export { default as Empty, EmptyProps } from './Empty'
-export { default as Button, ButtonProps } from './Button'
-export { default as Checkbox, CheckboxProps } from './Checkbox'
-export { default as Tooltip, TooltipProps } from './Tooltip'
-export { default as Select, SelectProps } from './Select'
-export { default as Radio, RadioProps } from './Radio'
-export { default as Tag, TagProps } from './Tag'
-export { default as Progress, ProgressProps } from './Progress'
-export { default as Avatar, AvatarProps } from './Avatar'
-export { default as AvatarGroup, AvatarGroupProps } from './AvatarGroup'
-export { default as Badge, BadgeProps } from './Badge'
-export { default as Popover } from './Popover'
-export { default as Notification, NotificationProps } from './Notification'
-export { default as Input, Search, InputProps, SearchProps } from './Input'
+export { default as Empty, EmptyProps } from './Empty';
+export { default as Button, ButtonProps } from './Button';
+export { default as Checkbox, CheckboxProps } from './Checkbox';
+export { default as Tooltip, TooltipProps } from './Tooltip';
+export { default as Select, SelectProps } from './Select';
+export { default as Radio, RadioProps } from './Radio';
+export { default as Tag, TagProps } from './Tag';
+export { default as Progress, ProgressProps } from './Progress';
+export { default as Avatar, AvatarProps } from './Avatar';
+export { default as AvatarGroup, AvatarGroupProps } from './AvatarGroup';
+export { default as Badge, BadgeProps } from './Badge';
+export { default as Popover } from './Popover';
+export { default as Notification, NotificationProps } from './Notification';
+export { default as Input, Search, InputProps, SearchProps } from './Input';
 export {
   default as Upload,
   UploadProps,
@@ -19,10 +19,14 @@ export {
   DraggerProps,
   UploadButtonContent,
   UploadButtonContentProps
-} from './Upload'
-export * from 'antd/lib/upload/interface'
-export { default as Alert, AlertProps, AlertWithButtons } from './Alert'
-export { default as Timeline, TimelineProps, TimelineItemProps } from './Timeline'
+} from './Upload';
+export * from 'antd/lib/upload/interface';
+export { default as Alert, AlertProps, AlertWithButtons } from './Alert';
+export {
+  default as Timeline,
+  TimelineProps,
+  TimelineItemProps
+} from './Timeline';
 export {
   default as DatePicker,
   MonthPicker,
@@ -31,11 +35,29 @@ export {
   TimePicker,
   RangePicker,
   QuarterPicker
-} from './DatePicker'
-export { default as Pagination, PaginationProps } from './Pagination'
-export { default as Message, MessageProps } from './Message'
-export { default as Drawer, DrawerProps } from './Drawer'
-export { default as PopConfirm, PopconfirmProps, NoIcon, WarningIcon } from './Popconfirm'
-export { default as AutoComplete, AutoCompleteProps } from './AutoComplete'
-export { default as Tabs, TabsProps, TabPaneProps, renderTabHeader } from './Tabs'
-export { default as Steps, Step, StepsProps, StepProps } from './Steps'
+} from './DatePicker';
+export { default as Pagination, PaginationProps } from './Pagination';
+export {
+  default as Menu,
+  MenuItem,
+  MenuItemGroup,
+  MenuProps,
+  MenuItemProps,
+  MenuItemGroupProps
+} from './Menu';
+export { default as Message, MessageProps } from './Message';
+export { default as Drawer, DrawerProps } from './Drawer';
+export {
+  default as PopConfirm,
+  PopconfirmProps,
+  NoIcon,
+  WarningIcon
+} from './Popconfirm';
+export { default as AutoComplete, AutoCompleteProps } from './AutoComplete';
+export {
+  default as Tabs,
+  TabsProps,
+  TabPaneProps,
+  renderTabHeader
+} from './Tabs';
+export { default as Steps, Step, StepsProps, StepProps } from './Steps';
