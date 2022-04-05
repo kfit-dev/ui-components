c312c1a9456d632a15bc7b97d4f5a97e7eda4af8
KelvinGKB <kengboonogoh@gmail.com>
2022-04-05 15:38:32 +0800

    Change description object to array

--- src/Descriptions/stories/index.stories.tsx
+++ src/Descriptions/stories/index.stories.tsx
@@ -11,54 +11,54 @@ export default {
 
 export const Basic = () =>{
 
-  const description = {
-    businessRegName : {
+  const description = [
+    {
       label : "Business registration name",
       content : "Puma Sdn Bhd"
     },
-    businessRegNumber: {
+    {
       label : "Business registration number",
       content : "11223344X"
     },
-    businessRegAddress: {
+    {
       label : "Business registration address",
       content : "Lot 123-A, Jalan Sungai Besar 45, Seksyen Sungai Besar, 67890 Sungai Besar, Kuala Lumpur."
     },
-    regNumber: {
+    {
       label : "SST/GST/VAT registration number",
       content : "123450000"
     },
-    businessBrandName: {
+    {
       label : "Business brand name",
       content : "Puma"
     },
-    businessType: {
+    {
       label : "Business type",
       content : "Online merchant (e-commerce)"
     },
-    website: {
+    {
       label : "Website",
       content : "www.puma.com.my"
     },
-    category: {
+    {
       label : "Category",
       content : "Retail"
     },
-    reportCategory: {
+    {
       label : "Reporting category",
       content : "Sportswear / Athletic"
     },
-    merchantMCC: {
+    {
       label : "Merchanr's MCC",
       content : "1000 - Fashion retail"
     }
-  }
+  ]
 
   return(
     <Descriptions title="Business Details" layout="vertical" colon={false}>
       {
-        Object.keys(description).map((Item,index) =>(
-          <DescriptionsItem key={index} label={description[Item].label}>{description[Item].content}</DescriptionsItem>
+        description.map((Item,index) =>(
+          <DescriptionsItem key={index} label={Item.label}>{Item.content}</DescriptionsItem>
         ))
       }
     </Descriptions>
