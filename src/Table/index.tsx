f83097c68ffe94363bb4a9e8670f78f48393605c
naimdanialjohar <37859609+naimdanialjohar@users.noreply.github.com>
2022-04-06 10:44:08 +0800

    new component table, added new packages, wip

--- src/Table/index.tsx
+++ src/Table/index.tsx
@@ -1,12 +1,56 @@
-import * as React from 'react'
-import { Table as AntTable, TableProps as AntTableProps } from 'antd'
+import * as React from 'react';
+import { Table as AntTable, TableProps as AntTableProps } from 'antd';
 
-import './style.css'
+import './style.css';
+import { ArrowsDownUp, CaretLeft, CaretRight, Funnel } from 'phosphor-react';
+import { ColumnType } from 'antd/lib/table';
+import { SortOrder } from 'antd/lib/table/interface';
+import { itemRender } from '../Pagination';
 
-export type TableProps<T> = AntTableProps<T>
+export type TableProps<T> = AntTableProps<T>;
+export const FilterIcon = <Funnel size={16} weight="light" />;
 
-const Table = <T extends {}>(props: TableProps<T>) => <AntTable {...props} />
+export const headerWithSort = <RecordType extends {}>(
+  sortColumns: {
+    column: ColumnType<RecordType>;
+    order: SortOrder;
+  }[],
+  title: string
+) => {
+  const sortable = sortColumns ? sortColumns.find(item => item.order) : '';
+  const sortClassname = sortable
+    ? sortable.order === 'ascend'
+      ? 'sort-ascend'
+      : 'sort-descend'
+    : '';
 
-Table.defaultProps = {}
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+      }}
+    >
+      <span>{title}</span>
+      <ArrowsDownUp className={sortClassname} size={16} weight="light" />
+    </div>
+  );
+};
+const Table = <T extends {}>(props: TableProps<T>) => {
+  return <AntTable {...props} />;
+};
 
-export default Table
+Table.defaultProps = {
+  locale: {
+    triggerDesc: 'Sort descending',
+    triggerAsc: 'Sort ascending',
+    cancelSort: 'Remove sorting'
+  },
+  pagination: {
+    position: ['bottomCenter'],
+    itemRender: itemRender(<CaretLeft size={16} />, <CaretRight size={16} />)
+  }
+};
+
+export default Table;
--- src/Table/stories/index.stories.tsx
+++ src/Table/stories/index.stories.tsx
@@ -1,13 +1,27 @@
-import React, { useState } from 'react'
-import { ComponentMeta, Story } from '@storybook/react'
-import { Space } from 'antd'
-import { TableRowSelection } from 'antd/lib/table/interface'
-
-import { parseISO, format as fnFormat, differenceInMilliseconds } from 'date-fns'
-
-import Button from '../../Button'
-import { default as Tag } from '../../Tag/index'
-import { default as Table, TableProps } from '../index'
+import React, { useState } from 'react';
+import { ComponentMeta, Story } from '@storybook/react';
+import { TableRowSelection } from 'antd/lib/table/interface';
+import {
+  SortableContainer,
+  SortableElement,
+  SortableHandle
+} from 'react-sortable-hoc';
+import { arrayMoveImmutable } from 'array-move';
+import {
+  parseISO,
+  format as fnFormat,
+  differenceInMilliseconds
+} from 'date-fns';
+import Button from '../../Button';
+import { default as Tag } from '../../Tag/index';
+import {
+  default as Table,
+  FilterIcon,
+  headerWithSort,
+  TableProps
+} from '../index';
+import { default as Tooltip } from '../../Tooltip';
+import { List } from 'phosphor-react';
 
 export default {
   title: 'Table',
@@ -26,9 +40,10 @@ export default {
       control: { type: 'check' }
     }
   }
-} as ComponentMeta<typeof Table>
+} as ComponentMeta<typeof Table>;
 
-const format = (date: string) => fnFormat(typeof date === 'string' ? parseISO(date) : date, 'dd MMM yyyy')
+const format = (date: string) =>
+  fnFormat(typeof date === 'string' ? parseISO(date) : date, 'dd MMM yyyy');
 
 enum SubmissionStatus {
   NotStarted,
@@ -37,124 +52,254 @@ enum SubmissionStatus {
 }
 
 type RecordType = {
-  key: string
-  submissionDate: string
-  merchantName: string
-  outletlocation: string
-  sbmissionStatus: SubmissionStatus
-}
+  key: string;
+  submissionDate: string;
+  merchantName: string;
+  outletLocation: string;
+  submissionStatus: SubmissionStatus;
+  productType: string;
+};
 
 type ArgsType = TableProps<RecordType> & {
-  label: string
-  checked?: string[]
-  withFooter: string[]
-}
+  label: string;
+  checked?: string[];
+  withFooter?: string[];
+};
 
 const dataSource = [
   {
     key: '1',
     submissionDate: new Date(2022, 1, 3).toISOString(),
     merchantName: 'Dunkin Donuts',
-    outletlocation: 'Subang Jaya',
-    sbmissionStatus: SubmissionStatus.NotStarted
+    outletLocation: 'Subang Jaya',
+    submissionStatus: SubmissionStatus.NotStarted,
+    productType: 'FavePay',
+    children: [
+      {
+        key: '13',
+        submissionDate: new Date(2022, 1, 3).toISOString(),
+        merchantName: 'Another Dunkin Donuts Shop!',
+        outletLocation: 'Subang Jaya',
+        submissionStatus: SubmissionStatus.NotStarted,
+        productType: 'FavePay'
+      }
+    ]
   },
   {
     key: '2',
     submissionDate: new Date(2022, 1, 1).toISOString(),
     merchantName: 'Puma Sdn Bhd',
-    outletlocation: 'One Pavilion',
-    sbmissionStatus: SubmissionStatus.NotStarted
+    outletLocation: 'One Pavilion',
+    submissionStatus: SubmissionStatus.NotStarted,
+    productType: 'FavePay'
   },
   {
     key: '3',
     submissionDate: new Date(2022, 1, 2).toISOString(),
     merchantName: 'ArtFriend',
-    outletlocation: 'Jaya One',
-    sbmissionStatus: SubmissionStatus.NotStarted
+    outletLocation: 'Jaya One',
+    submissionStatus: SubmissionStatus.NotStarted,
+    productType: 'FavePay'
+  },
+  {
+    key: '4',
+    submissionDate: new Date(2022, 1, 3).toISOString(),
+    merchantName: 'Dunkin Donuts 2',
+    outletLocation: 'One Pavilion, 123A, Jalan Pavilion, 47000 Kuala Lumpur.',
+    submissionStatus: SubmissionStatus.NotStarted,
+    productType: 'FavePay'
+  },
+  {
+    key: '5',
+    submissionDate: new Date(2022, 1, 1).toISOString(),
+    merchantName: 'Puma Sdn Bhd 2',
+    outletLocation: 'One Pavilion',
+    submissionStatus: SubmissionStatus.NotStarted,
+    productType: 'FavePay'
+  },
+  {
+    key: '6',
+    submissionDate: new Date(2022, 1, 2).toISOString(),
+    merchantName: 'ArtFriend 2',
+    outletLocation: 'Jaya One',
+    submissionStatus: SubmissionStatus.NotStarted,
+    productType: 'FavePay'
+  },
+  {
+    key: '7',
+    submissionDate: new Date(2022, 1, 3).toISOString(),
+    merchantName: 'Dunkin Donuts',
+    outletLocation: 'Subang Jaya',
+    submissionStatus: SubmissionStatus.NotStarted,
+    productType: 'FavePay'
+  },
+  {
+    key: '8',
+    submissionDate: new Date(2022, 1, 1).toISOString(),
+    merchantName: 'Puma Sdn Bhd',
+    outletLocation: 'One Pavilion',
+    submissionStatus: SubmissionStatus.NotStarted,
+    productType: 'FavePay'
+  },
+  {
+    key: '9',
+    submissionDate: new Date(2022, 1, 2).toISOString(),
+    merchantName: 'ArtFriend',
+    outletLocation: 'Jaya One',
+    submissionStatus: SubmissionStatus.NotStarted,
+    productType: 'FavePay'
+  },
+  {
+    key: '10',
+    submissionDate: new Date(2022, 1, 3).toISOString(),
+    merchantName: 'Dunkin Donuts 2',
+    outletLocation: 'One Pavilion, 123A, Jalan Pavilion, 47000 Kuala Lumpur.',
+    submissionStatus: SubmissionStatus.NotStarted,
+    productType: 'FavePay'
+  },
+  {
+    key: '11',
+    submissionDate: new Date(2022, 1, 1).toISOString(),
+    merchantName: 'Puma Sdn Bhd 2',
+    outletLocation: 'One Pavilion',
+    submissionStatus: SubmissionStatus.NotStarted,
+    productType: 'FavePay'
+  },
+  {
+    key: '12',
+    submissionDate: new Date(2022, 1, 2).toISOString(),
+    merchantName: 'ArtFriend 2',
+    outletLocation: 'Jaya One',
+    submissionStatus: SubmissionStatus.NotStarted,
+    productType: 'FavePay'
   }
-]
+];
 
 const nameFilters = dataSource.map(({ merchantName }) => {
-  return { text: merchantName, value: merchantName }
-})
+  return { text: merchantName, value: merchantName };
+});
 
 const args: ArgsType = {
   size: 'large',
   checked: undefined,
-  withFooter: ['Footer'],
+  // withFooter: ['Footer'],
   dataSource: dataSource,
   columns: [
     {
-      title: 'Submission Date',
+      title: ({ sortColumns }) =>
+        headerWithSort(sortColumns, 'Submission Date'),
       dataIndex: 'submissionDate',
-      render: text => <>{format(text)}</>,
-      sorter: ({ submissionDate: a }, { submissionDate: b }) => differenceInMilliseconds(parseISO(a), parseISO(b))
+      render: text => (text ? format(text) : ''),
+      sorter: ({ submissionDate: a }, { submissionDate: b }) =>
+        differenceInMilliseconds(parseISO(a), parseISO(b))
     },
     {
-      title: 'Merchant Name',
+      title: ({ sortColumns }) => headerWithSort(sortColumns, 'Merchant Name'),
       dataIndex: 'merchantName',
       sorter: ({ merchantName: a }, { merchantName: b }) => a.localeCompare(b),
+      filters: nameFilters,
       filterMode: 'tree',
       filterSearch: true,
-      filters: nameFilters,
-      onFilter: (value, { merchantName }) => merchantName === value
+      onFilter: (value, { merchantName }) => merchantName === value,
+      filterIcon: FilterIcon
     },
     {
-      title: 'Sbmission Status',
-      dataIndex: 'sbmissionStatus',
-      sorter: ({ sbmissionStatus: a }, { sbmissionStatus: b }) => a - b,
-      render: (_, { sbmissionStatus }) => (
-        <>{sbmissionStatus === SubmissionStatus.NotStarted && <Tag color={'default'}>Not started</Tag>}</>
+      title: 'Outlet Location',
+      dataIndex: 'outletLocation',
+      ellipsis: {
+        showTitle: false
+      },
+      render: address => (
+        <Tooltip placement="topLeft" title={address}>
+          {address}
+        </Tooltip>
       )
+    },
+    {
+      title: 'Submission Status',
+      dataIndex: 'submissionStatus',
+      render: (_, { submissionStatus: submissionStatus }) => (
+        <>
+          {submissionStatus === SubmissionStatus.NotStarted && (
+            <Tag color={'default'}>Not started</Tag>
+          )}
+        </>
+      )
+    },
+    {
+      title: 'Product',
+      dataIndex: 'productType',
+      render: productType => <Tag color={'default'}>{productType}</Tag>
     },
     {
       title: 'Action(s)',
       key: 'action',
       render: (_, { merchantName }) => (
-        <Space size="middle">
-          <a onClick={() => confirm(`cancel submission for ${merchantName}?`)}>Cancel Submission</a>
-        </Space>
-      )
+        <a onClick={() => confirm(`cancel submission for ${merchantName}?`)}>
+          Cancel Submission
+        </a>
+      ),
+      fixed: 'right'
     }
   ],
   label: 'Table'
-}
+};
 
-const Template: Story<ArgsType> = ({ checked, withFooter, ...restProps }: ArgsType, context) => {
-  const { setSelectedRows } = context
+const Template: Story<ArgsType> = (
+  { checked, withFooter, ...restProps }: ArgsType,
+  context
+) => {
+  const { setSelectedRows, columns, dataSource, components } = context;
 
   const rowSelection: TableRowSelection<RecordType> =
     checked?.length > 0
       ? {
           type: 'checkbox',
-          onChange: (selectedRowKeys: React.Key[], selectedRows: RecordType[]) => setSelectedRows(selectedRows)
+          onChange: (
+            selectedRowKeys: React.Key[],
+            selectedRows: RecordType[]
+          ) => restProps.rowSelection || setSelectedRows(selectedRows),
+          columnWidth: 40
         }
-      : undefined
+      : undefined;
 
-  const footerFn = withFooter?.length > 0 ? () => withFooter[0] : undefined
+  const footerFn = withFooter?.length > 0 ? () => withFooter[0] : undefined;
 
-  return <Table rowSelection={rowSelection} footer={footerFn} {...restProps} />
-}
+  return (
+    <Table
+      columns={restProps.columns || columns}
+      dataSource={restProps.dataSource || dataSource}
+      rowSelection={rowSelection}
+      footer={footerFn}
+      components={args.components || components}
+      {...restProps}
+    />
+  );
+};
 
-export const Default = Template.bind({})
+export const Default = Template.bind({});
 
 Default.args = {
   ...args
-}
+};
 
-export const Checked = Template.bind({})
+export const Checked = Template.bind({});
 
 Checked.args = {
   ...args,
   checked: ['checked']
-}
+};
 
 Checked.decorators = [
   Story => {
-    const [selectedRows, setSelectedRows] = useState<RecordType[]>([])
+    const [selectedRows, setSelectedRows] = useState<RecordType[]>([]);
 
     const handleClick = () =>
-      confirm(`Cancel [${selectedRows.length}] submissions: ${selectedRows.map(r => r.merchantName).join(', ')}?`)
+      confirm(
+        `Cancel [${selectedRows.length}] submissions: ${selectedRows
+          .map(r => r.merchantName)
+          .join(', ')}?`
+      );
 
     return (
       <div
@@ -176,6 +321,84 @@ Checked.decorators = [
         </Button>
         <Story setSelectedRows={setSelectedRows} />
       </div>
-    )
+    );
   }
-]
+];
+
+export const Expandable = Template.bind({});
+
+Expandable.args = {
+  ...args
+  // expandable: {
+  //   expandedRowRender: record => (
+  //     <p style={{ margin: 0 }}>{record.description}</p>
+  //   )
+  // }
+};
+
+export const FixedHeaderAndColumn = Template.bind({});
+
+FixedHeaderAndColumn.args = {
+  scroll: { y: '78vh', x: 1500 },
+  ...args
+};
+
+export const SortWithHandle = Template.bind({});
+
+const DragHandle = SortableHandle(() => <List size={16} weight="light" />);
+const sortWithHandleColumns = [
+  {
+    title: 'Sort',
+    dataIndex: 'sort',
+    width: 80,
+    className: 'drag-visible',
+    render: () => <DragHandle />
+  },
+  ...args.columns
+];
+
+SortWithHandle.decorators = [
+  Story => {
+    const [data, setData] = React.useState(dataSource);
+
+    const onSortEnd = ({ oldIndex, newIndex }) => {
+      if (oldIndex !== newIndex) {
+        const newData = arrayMoveImmutable(
+          [].concat(data),
+          oldIndex,
+          newIndex
+        ).filter(el => !!el);
+        setData(newData);
+      }
+    };
+
+    const SortableItem = SortableElement(props => <tr {...props} />);
+    const SortableBody = SortableContainer(props => <tbody {...props} />);
+    const DraggableContainer = props => (
+      <SortableBody
+        useDragHandle
+        disableAutoscroll
+        helperClass="row-dragging"
+        onSortEnd={onSortEnd}
+        {...props}
+      />
+    );
+
+    const DraggableBodyRow = ({ className, style, ...restProps }) => {
+      const index = data.findIndex(x => x.key === restProps['data-row-key']);
+      return <SortableItem index={index} {...restProps} />;
+    };
+    return (
+      <Story
+        columns={sortWithHandleColumns}
+        dataSource={data}
+        components={{
+          body: {
+            wrapper: DraggableContainer,
+            row: DraggableBodyRow
+          }
+        }}
+      />
+    );
+  }
+];
