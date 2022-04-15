b8902750cf8853979455a49c43874b4e56740ecc
naimdanialjohar <37859609+naimdanialjohar@users.noreply.github.com>
2022-04-15 14:59:48 +0800

    update spin component

--- src/Spin/index.tsx
+++ src/Spin/index.tsx
@@ -1,22 +1,27 @@
 import * as React from 'react'
 import { LoadingOutlined } from '@ant-design/icons'
 import { Spin as AntSpin, SpinProps as AntSpinProps } from 'antd'
+import { SpinSize } from 'antd/lib/spin'
 
 import './style.css'
 
 export type SpinProps = AntSpinProps
 
+const iconSize = (size: SpinSize | number) => {
+  if (size === 'small') return 14
+  else if (size === 'default') return 20
+  else if (size === 'large') return 32
+  else return size
+}
+
+export const SpinIcon: React.FC<{ size: SpinSize | number }> = props => {
+  const { size = 'default' } = props
+  return <LoadingOutlined style={{ fontSize: iconSize(size) }} />
+}
+
 const Spin: React.FC<SpinProps> = props => {
-  return <AntSpin {...props} />
-}
-
-export const SpinIcon: React.FC<{ size?: number }> = props => {
-  const { size = 24 } = props
-  return <LoadingOutlined style={{ fontSize: size }} />
-}
-
-Spin.defaultProps = {
-  indicator: <SpinIcon />
+  const { indicator, size = 'default', ...restProps } = props
+  return <AntSpin indicator={<SpinIcon size={size} /> || indicator} {...restProps} />
 }
 
 export default Spin
--- src/Spin/stories/index.stories.tsx
+++ src/Spin/stories/index.stories.tsx
@@ -5,7 +5,13 @@ import { default as Spin, SpinProps } from '../index'
 
 export default {
   title: 'Spin',
-  component: Spin
+  component: Spin,
+  argTypes: {
+    size: {
+      options: ['small', 'default', 'large'],
+      control: { type: 'radio' }
+    }
+  }
 } as ComponentMeta<typeof Spin>
 
 const Template: ComponentStory<typeof Spin> = (args: SpinProps) => <Spin {...args} />
