ecb173e95aac8e1718263ed1bf1f71923312ed29
Kelvin-GohKengBoon <97870401+Kelvin-GohKengBoon@users.noreply.github.com>
2022-05-11 10:46:32 +0800

    Upgrade/ant design version 4.20.0 (#99)
    * Fixed styling issues
    
    * change type for props
    
    * Change small card button hasButton to true
    
    * menu component fix
    
    * updated components to use menu items prop
    
    * removed deprecated exports
    
    * changed string to boolean
    
    * added defaultProp treeData to prevent component from using children as default
    
    * Update import for modal/breadcrum & input
    
    * [Develop] created new Comment component (#97)
    
    * created new component Comment
    
    * added new eslint configuration for switch cases indentation
    
    * removed commented code
    
    * update based on feedback by qa and designer
    
    * include comments length
    
    * refactor based on feedback
    
    * exported types
    
    * Fixed styling issues
    
    * change type for props
    
    * Change small card button hasButton to true
    
    * menu component fix
    
    * updated components to use menu items prop
    
    * removed deprecated exports
    
    * changed string to boolean
    
    * added defaultProp treeData to prevent component from using children as default
    
    * Add Import for TextAreaProps
    
    Co-authored-by: KelvinGKB <kengboonogoh@gmail.com>
    Co-authored-by: naimdanialjohar <37859609+naimdanialjohar@users.noreply.github.com>

--- src/index.tsx
+++ src/index.tsx
@@ -48,18 +48,7 @@ export {
   DatePickerProps,
   DateRangePickerProps
 } from './DatePicker'
-export {
-  isIconFill,
-  default as Menu,
-  MenuItem,
-  MenuItemGroup,
-  SubMenu,
-  MenuProps,
-  MenuItemProps,
-  MenuItemGroupProps,
-  SubMenuProps,
-  MenuDividerProps
-} from './Menu'
+export { default as Menu, isIconFill, MenuDivider, MenuProps, MenuDividerProps } from './Menu'
 export { default as Table, TableProps, FilterIcon, HeaderWithSort, HeaderWithSortProps } from './Table'
 export { default as Collapse, CollapsePanel, CollapseProps, CollapsePanelProps } from './Collapse'
 export { default as Message, MessageProps } from './Message'
