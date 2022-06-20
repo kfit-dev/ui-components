bd00ee07956fcf8c5293da7a5c39fd86eb88cdae
KelvinGKB <kengboonogoh@gmail.com>
2022-06-20 15:09:47 +0800

    FIx styling import and Optimize Code

--- src/Form/index.tsx
+++ src/Form/index.tsx
@@ -1,16 +1,13 @@
-import * as React from "react";
 import { Form as AntForm, FormProps as AntFormProps, FormItemProps as AntFormItemProps } from "antd";
 
 import { default as FormItemType } from 'antd/lib/form/FormItem';
+import { default as FormType } from 'antd/lib/form/Form';
 import "./style.css"
 
 export type FormProps = AntFormProps;
 export type FormItemProps = AntFormItemProps;
 
 export const FormItem : typeof FormItemType = AntForm.Item;
-
-const Form: React.FC<FormProps> = (props) => {
-  return <AntForm {...props} />;
-};
+const Form : typeof FormType= AntForm;
 
 export default Form;
