c316a570ec9f20eb19ac47bd453f4c87a4ba12d4
naimdanialjohar <37859609+naimdanialjohar@users.noreply.github.com>
2023-05-17 14:04:59 +0800

    [develop] exported pagination (#153)
    * exported pagination
    
    * added new mobile responsive media queries based on antd
    
    * remove margin in smaller screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,16 @@
 module.exports = {
   content: ['./src/**/*.tsx', './src/**/*.ts'],
-  presets: [require('fave-tailwindcss-presets')]
+  presets: [require('fave-tailwindcss-presets')],
+  theme: {
+    extend: {
+      screens: {
+        // overrides existing media queries
+        xs: {'min': '0px', 'max': '575px' },
+        sm: {'min': '576px', 'max': '767px' },
+        md: {'min': '768px', 'max': '991px' },
+        lg: {'min': '992px', 'max': '1199px' },
+        xl: {'min': '1200px'}
+      }
+    },
+  },
 }
